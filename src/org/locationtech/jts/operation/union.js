a8c9446ca9a0fb3acef78e4077629dc5138ca6dc
Trevor Hall <trevor.jacob.hall@gmail.com>
2017-01-17 12:56:49 -0600

    Added missing union classes

--- src/org/locationtech/jts/operation/union.js
+++ src/org/locationtech/jts/operation/union.js
@@ -1,5 +1,13 @@
+import CascadedPolygonUnion from './union/CascadedPolygonUnion'
+import PointGeometryUnion from './union/PointGeometryUnion'
 import UnaryUnionOp from './union/UnaryUnionOp'
+import UnionInteracting from './union/UnionInteracting'
+import UnionOp from './union/UnionOp'
 
 export {
-  UnaryUnionOp
+  CascadedPolygonUnion,
+  PointGeometryUnion,
+  UnaryUnionOp,
+  UnionInteracting,
+  UnionOp
 }
